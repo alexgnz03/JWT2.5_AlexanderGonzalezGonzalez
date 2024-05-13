c05e223540fd4289bd0f9d07ea088a877b8eca02
alexandereviden <alexander.gonzalez.external@eviden.com>
2024-05-13 16:07:22 +0100

    Initial commit

---
+++ src/main/java/com/alexander/jwtproject/User/Role.java
@@ -0,0 +1,6 @@
+package com.alexander.jwtproject.User;
+
+public enum Role {
+    ADMIN,
+    USER  
+}
---
+++ src/test/java/com/alexander/jwtproject/DemoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexander.jwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
